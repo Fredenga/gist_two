17a4fea483d2826436f4ddade1cef84b5904d607
fredenga <dengafred@gmail.com>
2025-03-31 10:26:50 -0700

    create footer

--- lib/gist_two_web/components/layouts/app.ex
+++ lib/gist_two_web/components/layouts/app.ex
@@ -4,7 +4,7 @@ defmodule GistTwoWeb.Layouts.App do
   def toggle_dropdown_menu do
     JS.toggle(
       to: "#dropdown_menu",
-      in: {"transition ease-out duration-100", "transform opacity-0 translate-y-[-10%]", "transform opacity-100 translation-y-0"},
+      in: {"transition ease-out duration-100", "transform opacity-0 translate-y-[-10%]", "transform opacity-100 translation-y-0"}
       # out: {"transition ease-in duration-75", "transform opacity-100 translate-y-0"}
       )
   end
